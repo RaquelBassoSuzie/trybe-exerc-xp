4efab01dfd33a8015f8682dbdd6376484b505980
elviraraquelbasso@gmail.com <elviraraquelbasso@gmail.com>
2021-11-18 09:55:52 -0500

    halfway

--- main.js
+++ main.js
@@ -25,10 +25,23 @@ thirdLi.addEventListener('click', handleChangeTech);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+input.addEventListener('dbclick', function() {
+window.location.replace('https://blog.betrybe.com/');
+});
+
+
+
+
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+input.addEventListener('dbclick', function() {
+    window.location.replace('https://github.com/RaquelBassoSuzie');
+    });
+    
+
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
