1c39180f34bd8c87d1a260dfb4e95a0ee365dea7
elviraraquelbasso@gmail.com <elviraraquelbasso@gmail.com>
2021-11-21 06:31:46 -0500

    final

--- ej53-race/script.js
+++ ej53-race/script.js
@@ -21,9 +21,45 @@
 //unction onDOMLoaded(e){
    // console.log('DOM carregado!: ');
 //}
-let bt = document.querySelector(' .hey');
+///let bt = document.querySelector(' .hey');
 
-bt.addEventListener( 'click' , onClicked )
-function onClicked(e){
-    console.log('clicou: ', e)
+//bt.addEventListener( 'click' , onClicked )
+//function onClicked(e){
+   // console.log('clicou: ', e)
+
+// GERAR NUMEROS ALEATÓREOS
+//let n = Math.round ( Math.random() * 60 )
+//console.log(n)
+//let width = '500px'
+// converter string em number
+//console.log(parseInt(width))
+/* COMO PEGAR O VALOR DA LARGURA DO BROWSER */
+//console.log( 'largura: ', window.innerWidth);
+let bt = document.querySelector(' .hey');
+let car1 = document.querySelector('.car1');
+let car2 = document.querySelector('.car2');
+
+
+bt.addEventListener('click', onClicked );
+
+car1.style.marginLeft = '3px';
+car2.style.marginLeft = '3px';
+
+
+function onClicked(){
+  
+  let currentPos = car1.style.marginLeft;
+  let random1 =( Math.random() * 20 )
+  console.log(random1)
+  let newPos = parseInt(currentPos) + random1;
+
+  let currentPos2 = car2.style.marginLeft;
+  let random2 =( Math.random() * 20 )
+  console.log(random2)
+  let newPos2 = parseInt(currentPos2) + random2;
+
+  
+
+  car1.style.marginLeft = newPos + 'px'
+  car2.style.marginLeft = newPos2 + 'px'
 }
